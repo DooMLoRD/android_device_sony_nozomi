29075c7b9acf34494d223f6cc49376c6f517b5f9
DooMLoRD <metoo.mememe@gmail.com>
2013-06-08 02:02:04 +0530

    TWRP: added flags to BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,3 +31,23 @@ TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p15
 
 TARGET_OTA_ASSERT_DEVICE := LT26i,nozomi
+
+
+#TWRP flags
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_FLASH_FROM_STORAGE := true
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p14"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "barrier=1,noauto_da_alloc"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/nozomi/recovery/graphics.c
+TW_BRIGHTNESS_PATH := /sys/devices/i2c-3/3-0040/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
